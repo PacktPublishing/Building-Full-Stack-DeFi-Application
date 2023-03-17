331ca6498b674737cef8d5b7b80e55896a515d50
Samuel Zhou <samuel.robota@gmail.com>
2023-03-17 11:27:29 -0700

    Update hardhat.config.js to support .env

--- defi-apps/hardhat.config.js
+++ defi-apps/hardhat.config.js
@@ -1,14 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-// Go to https://infura.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const INFURA_API_KEY = "c4246efc57c942c0b901ca4c8c75d8c4";
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "ca1d8855773f2bff7933ad6e5a7e247108ffcfa4392bce271542fe4e15217e15";
+const GOERLI_API_URL = process.env.API_URL;
+const GOERLI_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.17",
@@ -20,7 +14,7 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      url: GOERLI_API_URL,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   }
