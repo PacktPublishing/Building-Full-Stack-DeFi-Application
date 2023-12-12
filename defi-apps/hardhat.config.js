d092a6eab9a07a804d3aa0709515519cb3ace80e
Samuel Zhou <samuel.robota@gmail.com>
2023-12-11 23:08:51 -0800

    Update Goerli with Sepolia

--- defi-apps/hardhat.config.js
+++ defi-apps/hardhat.config.js
@@ -6,7 +6,15 @@ const SEPOLIA_API_URL = process.env.API_URL;
 const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   paths: {
     sources: "./src/backend/contracts",
     artifacts: "./src/backend/artifacts",
